ddfa4e1b3a19444e6af3f41fd688e37074a82655
Mihailo Škorić <31268468+procesaur@users.noreply.github.com>
2023-05-16 13:25:25 +0200

    Update redisworks.py

--- redisworks.py
+++ redisworks.py
@@ -19,7 +19,7 @@ def redis_queue():
         return None
     else:
         conn = redis_conn()
-        q = Queue(connection=conn, default_timeout=600)
+        q = Queue(connection=conn, default_timeout=3600)
         return q
 
 
